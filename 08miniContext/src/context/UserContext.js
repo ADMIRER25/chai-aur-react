d6a4e0aca1dce411db8c950a231c49284f5a85a1
Subha Nayak <subhanayak022@gmail.com>
2024-04-06 20:58:32 +0530

    comment

--- 08miniContext/src/context/UserContext.js
+++ 08miniContext/src/context/UserContext.js
@@ -4,3 +4,11 @@ const UserContext = React.createContext()
 
 export default UserContext;
 
+
+{/* <UserContext>
+    <Login/>
+    <Card>
+        <Data/>
+    </Card>
+</UserContext> */}
+// Whenever we wrap the part using UserContext this part is a provider and now it can access the global data or we can say UserContext or we can say  LIke Data can Access every state through UserContext
