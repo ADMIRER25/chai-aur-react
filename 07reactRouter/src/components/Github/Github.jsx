80be4491ec9729eec2c244135504b9f8a5568378
Subha Nayak <subhanayak022@gmail.com>
2024-04-08 12:00:35 +0530

    comment

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -1,20 +1,20 @@
 import React, { useEffect, useState } from "react";
-// import { useLoaderData } from 'react-router-dom'
+import { useLoaderData } from 'react-router-dom'
 
 function Github() {
-  // const data = useLoaderData() // je data ta return hochhe eta setake save kore return korabe 
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    fetch("https://api.github.com/users/hiteshchoudhary")
-      .then(response=> response.json())
-      .then((data) => {
-        console.log(data);
-        setData(data);
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
-  }, []);
+  const data = useLoaderData() // je data ta return hochhe eta setake save kore return korabe 
+  // const [data, setData] = useState([]);
+  // useEffect(() => {
+  //   fetch("https://api.github.com/users/hiteshchoudhary")
+  //     .then(response=> response.json())
+  //     .then((data) => {
+  //       console.log(data);
+  //       setData(data);
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error fetching data:", error);
+  //     });
+  // }, []);
 
   return (
     <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl">
@@ -26,7 +26,7 @@ function Github() {
 
 export default Github;
 
-// export const githubInfoLoader = async () => {
-//     const response = await fetch('https://api.github.com/users/hiteshchoudhary')
-//     return response.json()
-// }
+export const githubInfoLoader = async () => {
+    const response = await fetch('https://api.github.com/users/hiteshchoudhary')
+    return response.json()
+}
